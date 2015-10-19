979a1290eaeeec49f9b1621fdca9f061f6655eb7
Anthony <737749893@qq,com>
2015-10-19 14:22:38 +0800

    加入显示天气信息的功能。

--- src/com/Anthony/travelsweather/receiver/AutoUpdateReceiver.java
+++ src/com/Anthony/travelsweather/receiver/AutoUpdateReceiver.java
@@ -1,5 +1,14 @@
 package com.Anthony.travelsweather.receiver;
 
-public class AutoUpdateReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
+public class AutoUpdateReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateReceiver.class);
+		context.startService(i);
+	} 
 }
